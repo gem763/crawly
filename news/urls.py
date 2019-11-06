1b4ec5a8b17260534b8a279c295bd4def97bb214
gem763 <16988092+gem763@users.noreply.github.com>
2019-11-06 21:19:31 +0900

    .

--- news/urls.py
+++ news/urls.py
@@ -3,5 +3,6 @@ from . import views as v
 
 urlpatterns = [
     path('update/', v.update, name='update'),
+    path('newscrawl_and_bigquerize/', v.newscrawl_and_bigquerize, name='newscrawl_and_bigquerize'),
     path('test/', v.test, name='test'),
 ]
--- news/views.py
+++ news/views.py
@@ -1,26 +1,38 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect, JsonResponse
 from newscrawler import NewsCrawler
-from background_task import background
+#from background_task import background
 
 # Create your views here.
 
-@background(schedule=0)
-def crawl_and_update():
-    crawler = NewsCrawler(storage='bigquery')
-    crawler.collect()
-    crawler.crawl()
-    #print(crawler.crawl_summary)
-    crawler.record()
-    crawler.report()
+# @background(schedule=0)
+# def crawl_and_update():
+#     crawler = NewsCrawler(storage='bigquery')
+#     crawler.collect()
+#     crawler.crawl()
+#     crawler.record()
+#     crawler.report()
+
+def newscrawl_and_bigquerize(request):
+# def update(request):
+    try:
+        crawler = NewsCrawler(storage='bigquery')
+        crawler.collect()
+        crawler.crawl()
+        crawler.record()
+        crawler.report()
+        # print('**************************************')
+        return JsonResponse({'result':crawler.crawl_summary.to_html()})
+
+    except ConnectionAbortedError as e:
+        print('connection aborted')
+
+    except Exception as e:
+        return JsonResponse({'result':e})
 
 
 def update(request):
-    try:
-        crawl_and_update()
-        return HttpResponse('requested')
-    except Exception as e:
-        return HttpResponse(e)
+    return render(request, 'news/update.html')
 
 
 def test(request):
